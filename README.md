df07e50e067573a742bb9d9f52e7a5f832254c54
agiz <agiz@localhost>
2015-04-25 19:55:31 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# auto repository
+
+Semiautomatically create remote github/bitbucket repository.
+
+## What is this?
+
+This script will create remote repository of your current project.
+
+More explicit, it will:
+- Create remote repository either on github or bitbucket.
+- Initialize your project (`git init)`.
+- Add remote repository (`git remote add origin <provider>`).
+- Add all files (`git add .`).
+- Commit all changes (`git commit -a -m 'Initial commit.'`).
+- Push your project to the remote provider (`git push -u origin master`).
+
+Script only works on vanilla projects without `.git` directory.
+If such directory alread exists, scipt will terminate without
+making any modifications.
+
+## What this is not.
+
+This script will not update your project once initial push is completed.
+
+## Instalation
+
+1. Put `auto_repository.sh` somewhere along the executable PATH. `/usr/local/bin` for example.
+2. Make script executable `chmod u+x /usr/local/bin/auto_repository.sh`.
+
+## Configuration
+
+Change `USER` variable in `auto_repository.sh` and you are set.
+Default provider is bitbucket and default repository visibility
+for bitbucket is `PRIVATE`. Default visibility for github is
+`PUBLIC`. Change according to your needs.
+
+## Usage
+
+1. Navigate to the project you want to upload: `cd ~/Projects/my_new_project`.
+2. Run `auto_repository.sh <bitbucket|github>` in project's directory. Bitbucket
+is default provider if no arguments are supplied.
+3. You will be prompted to enter a password.
+4. There is nothing more to say.
+
+## Credits
+
+[agiz](https://github.com/agiz)
+
+## License
+
+TODO: Write license
---
+++ auto_repository.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+#set -e
+# do not use that! handle errors by yourself.
+
+
+#
+# user set variables
+#
+
+USER=agiz
+# remote username
+
+PRIVATE="true"
+# private repository by default
+
+REPO="${PWD##*/}"
+# extracts name of the project from path
+# ~/foo/bar/prjectname => REPO == projectname
+
+PROVIDER=""
+# provider name
+
+PROVIDER_URL=""
+# remote provider
+
+PROVIDER_ORIGIN=""
+# remote provider origin
+
+PROVIDER_ORIGIN_1="git@bitbucket.org:${USER}/${REPO}.git"
+PROVIDER_ORIGIN_2="git@github.com:${USER}/${REPO}.git"
+# origin of remote provider
+
+
+#
+# parse input arguments
+#
+
+if [ "$#" -eq "0" ]; then
+  PROVIDER="bitbucket"
+  PROVIDER_ORIGIN="${PROVIDER_ORIGIN_1}"
+  PROVIDER_URL="https://api.bitbucket.org/1.0/repositories -d name=${REPO}&is_private=${PRIVATE}"
+else
+  PROVIDER="${1}"
+  case "${PROVIDER}" in
+    "bitbucket")
+      PROVIDER_ORIGIN="${PROVIDER_ORIGIN_1}"
+      PROVIDER_URL="https://api.bitbucket.org/1.0/repositories -d name=${REPO}&is_private=${PRIVATE}" ;;
+    "github")
+      PRIVATE="false"
+      PROVIDER_ORIGIN="${PROVIDER_ORIGIN_2}"
+      PROVIDER_URL="https://api.github.com/user/repos -d {\"name\":\"${REPO}\"}" ;;
+    *)
+      echo "ERROR: Unknown provider - ${PROVIDER}."
+      echo "Usage: Navigate to the project, execute: $0 <bitbucket|github>"
+      exit 1 ;;
+  esac
+fi
+
+
+#
+# single exit - return with error
+#
+
+err_ret() {
+  cd -
+  # back to initial directory
+
+  exit ${1}
+  #return with error code
+}
+
+
+echo "Creating ${REPO} on ${PROVIDER} as [PRIVATE=${PRIVATE}]."
+echo ""
+
+cd "${PWD}"
+# move to project's directory
+
+git status >/dev/null 2>&1
+# check if this project already has it's repository
+
+if [ "$?" -eq "0" ]; then
+  echo "This project already has repository!"
+  err_ret 1
+fi
+
+DATA=`curl -k -X POST --user ${USER} ${PROVIDER_URL}`
+# create repository (needs user input for password)
+
+if [[ "${DATA}" != *"\"name\": \"${REPO}\""* ]]; then
+  # both bitbucket and github share "name": "REPO" string in the output.
+  echo "ERROR: Something went wrong."
+  echo "Check your password or make sure remote repository with that name does not exist."
+  err_ret 1
+fi
+
+echo "Initializing repository..."
+git init
+echo ""
+
+echo "Adding remote repository..."
+git remote add origin "${PROVIDER_ORIGIN}"
+echo ""
+
+echo "Adding all files..."
+git add .
+echo ""
+
+echo "Commiting changes..."
+git commit -a -m 'Initial commit.'
+echo ""
+
+echo "Pushing project to remote repository..."
+git push -u origin master
+echo ""
+
+cd -
+# back to initial directory
+
